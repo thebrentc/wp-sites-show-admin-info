e759c8b5c090ca5ab43f973360c3439b5428a538
thebrentc <thebrentc@gmail.com>
2019-10-01 00:25:19 +0100

    Add lookup for all administrator emails

--- wp_admin_emails.js
+++ wp_admin_emails.js
@@ -3,31 +3,7 @@
  * Paste code into developer tools console, then call lookupAdminEmails(); and optionally exportTableAsCSV(jQuery('table.wp-list-table'));
  * @author thebrentc@gmail.com 
  */
-function lookupAdminEmail(siteId, e) {
-    jQuery.ajax({
-        url: "https://" + window.location.hostname + "/wp/wp-admin/network/site-settings.php",
-        method: "GET",
-        data: { id : siteId },
-    })
-      .done(function( html ) {
-          // e.g. s/id="admin_email" value="someone@example.com"/
-          var matches = html.match(/id="admin_email" value="([^"]*)"/);
-          var adminEmail = matches[1];
-          //console.log(adminEmail);
-          //return adminEmail;
-          jQuery(e).append('<td class = "admin-email">'+adminEmail+'</td>');
-    });
-}
-
-function lookupAdminEmails() {
-    jQuery('table.wp-list-table thead tr').append('<th id = "admin-email">Admin email</th>');
-    jQuery('table.wp-list-table tbody tr').each(function() {
-        var siteId = jQuery(this).find('.object_id').first().text();
-        //console.log(siteId);
-        lookupAdminEmail(siteId, this);         
-    });
-}
-
+  
 if (!String.prototype.trim) {
   String.prototype.trim = function () {
     return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
@@ -39,6 +15,60 @@ if (!String.prototype.replaceAll) {
         var target = this;
         return target.replace(new RegExp(search, 'g'), replacement);        
     };
+} 
+  
+/* Lookup only main site admin email and add column */
+function lookupSiteAdminEmail(siteId, e) {
+    jQuery.ajax({
+        url: "https://" + window.location.hostname + "/wp/wp-admin/network/site-settings.php",
+        method: "GET",
+        data: { id : siteId },
+    })
+      .done(function( html ) {
+          // e.g. s/id="admin_email" value="someone@example.com"/
+          var matches = html.match(/id="admin_email" value="([^"]*)"/);
+          var adminEmail = matches[1];
+          jQuery(e).append('<td class = "admin-email">'+adminEmail+'</td>');
+    });
+}
+
+/* Lookup all main site admin email and add columns */
+function lookupSiteAdminEmails() {
+    jQuery('table.wp-list-table thead tr').append('<th id = "admin-email">Admin email</th>');
+    jQuery('table.wp-list-table tbody tr').each(function() {
+        var siteId = jQuery(this).find('.object_id').first().text();
+        //console.log(siteId);
+        lookupSiteAdminEmail(siteId, this);
+    });
+}
+
+/* Lookup all administrator emails for site into column */
+function lookupSiteAdministratorEmails(siteId, e) {
+    jQuery.ajax({
+        url: "https://" + window.location.hostname + "/wp/wp-admin/network/site-users.php",        
+        method: "GET",
+        data: { id : siteId, role: 'administrator' },
+    })
+      .done(function( html ) {
+          var matches = html.matchAll(/<td.*data-colname="Email"><a[^>]*>(.*)<\/a>/g);
+          var adminEmails = [];
+          for (const match of matches) {
+              adminEmails.push(match[1]);
+          }          
+          adminEmails_ = adminEmails.toString();
+          adminEmails_ = adminEmails_.replaceAll(',',';');
+          jQuery(e).append('<td class = "administrators">'+adminEmails.length+'</td>'); // add count
+          jQuery(e).append('<td class = "administrator-emails">'+adminEmails_+'</td>');
+    });
+}
+
+function lookupAllSiteAdministratorEmails() {
+    jQuery('table.wp-list-table thead tr').append('<th id = "administrators">Administrators</th>');
+    jQuery('table.wp-list-table thead tr').append('<th id = "administrator-emails">Administrator emails</th>');    
+    jQuery('table.wp-list-table tbody tr').each(function() {
+        var siteId = jQuery(this).find('.object_id').first().text();        
+        lookupSiteAdministratorEmails(siteId, this);
+    });
 }
 
 var csv;
@@ -62,5 +92,6 @@ function exportTableAsCSV(table /* e.g. jQuery('table.wp-list-table') */) {
     console.log(csv);
 }
 
-// lookupAdminEmails();
+// lookupSiteAdminEmails();
+// lookupAllSiteAdministratorEmails();
 // exportTableAsCSV(jQuery('table.wp-list-table'));
